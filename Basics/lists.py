7a259ba989df9c388a3dcdc750bc3960554dcf19
Marcos Silveira Gonzalez Júnior <msilveiragjunior@outlook.com>
2025-10-30 18:48:22 -0300

    Commit_3
    PEP 8 Conventions

--- Basics/lists.py
+++ Basics/lists.py
@@ -1,20 +1,27 @@
-# In python, we have to remember that a list is represented by the hooks symbol ([]).
+# In python, we have to remember that a list is represented by the hooks
+#  symbol ([]).
 # So this countries = ['France', 'Guinea','Egypt'] is defined as a list
-list_1 = ['france', 'guinea','egypt']
-# With the command print(list_1[0]) we can access the first item of a list, that is: france
+list_1 = ['france', 'guinea', 'egypt']
+# With the command print(list_1[0]) we can access the first item of a list,
+#  that is: france
 print(list_1[0])
-# With the method .title - i.e. the dot defining the method followed by the name of the method that is associated with a function - we give a prettier
+# With the method .title - i.e. the dot defining the method followed by the
+#  name of the method that is associated with a function - we give a prettier
 # output than before, making the first letter be in uppercase
 print(list_1[0].title)
-# In python, we can access the last item of a list using -1 between the hooks symbol
+# In python, we can access the last item of a list using -1 between the hooks
+#  symbol
 print(list_1[-1].title)
-# We can also define numbers other than -1, that being: -2; -3; -4. By doing so, you'll output de second item from the end, the third and the forth and so on
-# We can concatenate a string with an item from a list with the plus (+) symbol in a variable, then print it using print
+# We can also define numbers other than -1, that being: -2; -3; -4. By doing
+#  so, you'll output de second item from the end, the third and the forth and
+#  so on
+# We can concatenate a string with an item from a list with the plus (+)
+#  symbol in a variable, then print it using print
 message = "I'm from " + list_1[0].title()
 print(message)
 
 # ----- Adding, altering and removing items from a list ----- #
-#Let's create another list and add some items to it
+# Let's create another list and add some items to it
 list_2 = ["ducati", "yamaha", "honda"]
 print(list_2)
 # We can modify elements in a dynamic list like this
@@ -22,9 +29,11 @@ list_2[0] = "honda"
 print(list_2)
 
 # --- Adding items to a list --- #
-# We can concatenate an item to a list to add items to it. The straightforward way to do it is by using the method .append('item_to_be_added')
+# We can concatenate an item to a list to add items to it. The straightforward
+# way to do it is by using the method .append('item_to_be_added')
 list_2.append('ducati')
-# We can also create an empty list and append items to it using the same method. It's simple
+# We can also create an empty list and append items to it using the same
+# method. It's simple
 list_3 = []
 list_3.append('ducati')
 list_3.append('honda')
@@ -32,7 +41,8 @@ list_3.append('yamaha')
 print(list_3)
 
 # --- Inserting elements to a list --- #
-# We can also insert elements to a list using the method .insert(0*, 'item_to_be_added')
+# We can also insert elements to a list using the method .insert(0*,
+#  'item_to_be_added')
 # *we can define the position in the list where the item will be added
 list_3.insert(0, 'suzuki')
 
@@ -42,18 +52,22 @@ del list_3[1]
 # This will remove the second item from the list
 # You can define any number and remove any item from any position that exists
 # --- Using the method .pop() to remove items --- #
-# With the method .pop() we can still work with the item from the list, as it is stored in a variable. It will remove the last item from the list
-# If you define the indice of the item that you want to remove, you can remove the item in any position with the method .pop
+# With the method .pop() we can still work with the item from the list,
+#  as it is stored in a variable. It will remove the last item from the list
+# If you define the indice of the item that you want to remove, you can
+#  remove the item in any position with the method .pop
 motorcycles_pop = list_3.pop()
 
 # --- Removing items according to a value --- #
-# With the .remove('name_of_the_item_in_the_list') you can remove an item from a list according to a value
+# With the .remove('name_of_the_item_in_the_list') you can remove an item
+#  from a list according to a value
 list_2.remove('yamaha')
 print(list_2)
 
 # ----- Organizing a list ----- #
 # Ordering permanently a list with the method .sort()
-# The method sort() will sort a list in ascending order by default and returns None as a result. The sort method is permanent
+# The method sort() will sort a list in ascending order by default and returns
+#  None as a result. The sort method is permanent
 list_2.sort()
 # We can also use arguments to reverse the method sort()
 list_2.sort(reverse=True)
@@ -65,11 +79,12 @@ print(sorted(list_1))
 # arguments like reverse=True can also be used
 
 # --- Reversing a list with the method .reverse() --- #
-# The .reverse() simply reverse the order of the original list, it does not organize it in a descending order
+# The .reverse() simply reverse the order of the original list, it does not
+#  organize it in a descending order
 list_3.reverse()
 # It will permanently modify the list
 
 # --- Discovering the size of a list --- #
 # we can discover the size of a list with the len function
 print(len(list_3))
-# ----------------------------------------------------------------------------------------- #
+# --------------------------------------------------------------------------- #
--- Basics/lists_working_with_them.py
+++ Basics/lists_working_with_them.py
@@ -1,40 +1,51 @@
-#Let's work with lists, following the Python Crash Course book by Eric Mathes
+# Let's work with lists, following the Python Crash Course book by Eric Mathes
 # --- Lets create a variable to work with --- #
 magicians = ['alice', 'david', 'carolina']
 # We will learn to work with the for loop
 for magician in magicians:
     print(magician)
-# In this loop the for loop will verify the if the conditions of the loop returns a True
-# If so, the loop body runs, returning every item from magicians, temporarily, storing inside magician and printing it.
-# It will continue running until the returning condition is false, in this case, None, or nothing in the list magicians is stored anylonger - nothing in the position
+# In this loop the for loop will verify the if the conditions of the loop
+# returns a True
+# If so, the loop body runs, returning every item from magicians, temporarily,
+# storing inside magician and printing it.
+# It will continue running until the returning condition is false, in this
+# case, None, or nothing in the list magicians is stored anylonger - nothing
+#  in the position
 # that currently is.
 
-# Indenting is very important when using Python. Because of that, we can do other things after for loops
+# Indenting is very important when using Python. Because of that, we can
+# do other things after for loops
 print(sorted(magicians))
 
 # --- Working with numbers --- #
-# We can use the range(*number,*number) to show a series of numbers inside a for loop
-for value in range(1,10):
+# We can use the range(*number,*number) to show a series of numbers
+# inside a for loop
+for value in range(1, 10):
     print(value)
-# The range function will now print the last number. The last number is non-inclusive
+# The range function will now print the last number. The last number is
+# non-inclusive
 
 # --- Creating a list of numbers --- #
-# We can create a list of numbers using the function list() with the range() function
-numbers = list(range(1,11))
+# We can create a list of numbers using the function list() with the
+# range() function
+numbers = list(range(1, 11))
 print(numbers)
 # Let's create an even number list
-even_numbers = list(range(2,11,2))
-# This will create a list beggining with 2 and, with the last argument (2), we will sum 2 to every number in the list created
+even_numbers = list(range(2, 11, 2))
+# This will create a list beggining with 2 and, with the last argument (2),
+# we will sum 2 to every number in the list created
 print(even_numbers)
-# We can also create a list with perfect squares using the exponential symbol **
+# We can also create a list with perfect squares using the exponential
+# symbol **
 squares = []
-for values in range(1,16):
+for values in range(1, 16):
     squares.append(values**2)
 print(squares)
 
 # --- Wroking with simple statistics functions in python --- #
-# We can use the min() function to find the minimum value in a list, max() to find the maximum value and sum() to find the sum of all the values of a list
+# We can use the min() function to find the minimum value in a list, max() to
+# find the maximum value and sum() to find the sum of all the values of a list
 list_1 = []
-for values in range(1,21):
+for values in range(1, 21):
     list_1.append(values)
 print(min(list_1), max(list_1), sum(list_1))
